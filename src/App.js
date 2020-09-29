fc185be10028662f6f5748a231c22c704064a4ee
andreyudi10 <andrianus.yudi@gmail.com>
2020-09-29 19:11:11 +0700

    create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="teamh-frontend">
+ 	   Hello World
+    </div>
+  );
+}
+
+export default App;
