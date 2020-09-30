0e9ddbd047696f16ed1460c5f25f6f0c5d40831a
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-09-30 09:53:06 +0700

    add react router, browse movies, and category navigation

---
+++ src/components/BrowseMovie/BrowseMovie.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import styles from "./BrowseMovie.module.css";
+import CategorySelect from "./CategorySelect/CategorySelect";
+
+const BrowseMovie = () => {
+  const [categories, setCategories] = useState([
+    "all",
+    "action",
+    "comedy",
+    "horor",
+    "anime",
+  ]);
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Browse by category</h2>
+      <CategorySelect categories={categories} />
+      <Route path="/browse/:category"></Route>
+    </div>
+  );
+};
+
+export default BrowseMovie;
---
+++ src/components/BrowseMovie/MovieList/MovieList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MovieList = () => {
+  return <div>This is Movie List</div>;
+};
+
+export default MovieList;
