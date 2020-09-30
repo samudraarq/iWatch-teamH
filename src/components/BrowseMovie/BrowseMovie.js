3a23e444a6576193f24aca22062357401b776e52
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-09-30 10:15:32 +0700

    movie list

--- src/components/BrowseMovie/BrowseMovie.js
+++ src/components/BrowseMovie/BrowseMovie.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import styles from "./BrowseMovie.module.css";
 import CategorySelect from "./CategorySelect/CategorySelect";
+import MovieList from "./MovieList/MovieList";
 
 const BrowseMovie = () => {
   const [categories, setCategories] = useState([
@@ -16,7 +17,9 @@ const BrowseMovie = () => {
     <div className={styles.container}>
       <h2 className={styles.title}>Browse by category</h2>
       <CategorySelect categories={categories} />
-      <Route path="/browse/:category"></Route>
+      <Route path="/movies/:category">
+        <MovieList />
+      </Route>
     </div>
   );
 };
--- src/components/BrowseMovie/MovieList/MovieList.js
+++ src/components/BrowseMovie/MovieList/MovieList.js
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import styles from "./MovieList.module.css";
 
 const MovieList = () => {
-  return <div>This is Movie List</div>;
+  const [movies, setMovies] = useState([
+    {
+      img: "https://picsum.photos/200/300",
+      title: "batman",
+      genre: "action",
+    },
+    {
+      img: "https://picsum.photos/200/300",
+      title: "batman",
+      genre: "action",
+    },
+    {
+      img: "https://picsum.photos/200/300",
+      title: "batman",
+      genre: "action",
+    },
+    {
+      img: "https://picsum.photos/200/300",
+      title: "batman",
+      genre: "action",
+    },
+    {
+      img: "https://picsum.photos/200/300",
+      title: "batman",
+      genre: "action",
+    },
+    {
+      img: "https://picsum.photos/200/300",
+      title: "batman",
+      genre: "action",
+    },
+    {
+      img: "https://picsum.photos/200/300",
+      title: "batman",
+      genre: "action",
+    },
+    {
+      img: "https://picsum.photos/200/300",
+      title: "batman",
+      genre: "action",
+    },
+    {
+      img: "https://picsum.photos/200/300",
+      title: "batman",
+      genre: "action",
+    },
+    {
+      img: "https://picsum.photos/200/300",
+      title: "batman",
+      genre: "action",
+    },
+    {
+      img: "https://picsum.photos/200/300",
+      title: "batman",
+      genre: "action",
+    },
+    {
+      img: "https://picsum.photos/200/300",
+      title: "batman",
+      genre: "action",
+    },
+    {
+      img: "https://picsum.photos/200/300",
+      title: "batman",
+      genre: "action",
+    },
+    {
+      img: "https://picsum.photos/200/300",
+      title: "batman",
+      genre: "action",
+    },
+    {
+      img: "https://picsum.photos/200/300",
+      title: "batman",
+      genre: "action",
+    },
+  ]);
+
+  const { category } = useParams();
+
+  const movRender = movies.map((movie, idx) => (
+    <Link to="/" key={idx} className={styles.movie}>
+      <img src={movie.img} alt="movie poster" className={styles.image} />
+      <p className={styles.title}>{movie.title}</p>
+      <p className={styles.genre}>{movie.genre}</p>
+    </Link>
+  ));
+
+  return <div className={styles.listContainer}>{movRender}</div>;
 };
 
 export default MovieList;
