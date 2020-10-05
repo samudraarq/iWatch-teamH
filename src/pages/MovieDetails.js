6a9b6fbbc85dd89886c9261cf7f1f2b1ce911ca6
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-05 20:40:01 +0700

    Movie Details : fetch movie details

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import DetailsHeader from "../components/DetailsHeader/DetailsHeader";
 import DetailsContent from "../components/DetailsContent/DetailsContent";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 const MovieDetails = () => {
+  const [movie, setMovie] = useState({});
+
   const { id } = useParams();
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const res = await axios.get(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_API_KEY}`
+      );
+      const result = await res.data;
+      console.log(result);
+      setMovie(result);
+    };
+    getMovies();
+  }, []);
+
   return (
     <div>
-      <DetailsHeader />
-      <DetailsContent movId={id} />
+      <DetailsHeader movie={movie} />
+      <DetailsContent movId={id} movie={movie} />
     </div>
   );
 };
