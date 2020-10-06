51451d4eb3dcb7b3503764099f186837d3db4fed
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-06 07:56:20 +0700

    Details Char : Get casts

--- src/components/DetailsContent/DetailsChar/DetailsChar.js
+++ src/components/DetailsContent/DetailsChar/DetailsChar.js
@@ -1,64 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 import styles from "./DetailsChar.module.css";
 
 const DetailsChar = () => {
-  const [chars, setChars] = useState([
-    {
-      name: "Samud",
-      img: "https://picsum.photos/200/300",
-    },
-    {
-      name: "Samud",
-      img: "https://picsum.photos/200/300",
-    },
-    {
-      name: "Samud",
-      img: "https://picsum.photos/200/300",
-    },
-    {
-      name: "Samud",
-      img: "https://picsum.photos/200/300",
-    },
-    {
-      name: "Samud",
-      img: "https://picsum.photos/200/300",
-    },
-    {
-      name: "Samud",
-      img: "https://picsum.photos/200/300",
-    },
-    {
-      name: "Samud",
-      img: "https://picsum.photos/200/300",
-    },
-    {
-      name: "Samud",
-      img: "https://picsum.photos/200/300",
-    },
-    {
-      name: "Samud",
-      img: "https://picsum.photos/200/300",
-    },
-    {
-      name: "Samud",
-      img: "https://picsum.photos/200/300",
-    },
-    {
-      name: "Samud",
-      img: "https://picsum.photos/200/300",
-    },
-    {
-      name: "Samud",
-      img: "https://picsum.photos/200/300",
-    },
-  ]);
+  const [chars, setChars] = useState([]);
 
   const { id } = useParams();
 
-  const listChar = chars.map((char, idx) => (
-    <div key={idx} className={styles.chars}>
-      <img src={char.img} alt={char.name} className={styles.image} />
+  useEffect(() => {
+    const getMovies = async () => {
+      const res = await axios.get(
+        `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${process.env.REACT_APP_TMDB_API_KEY}`
+      );
+      const result = await res.data;
+      console.log(result.cast);
+      setChars(result.cast);
+    };
+    getMovies();
+  }, []);
+
+  const listChar = chars.map((char) => (
+    <div key={char.id} className={styles.chars}>
+      <img
+        src={"https://image.tmdb.org/t/p/w500/" + char.profile_path}
+        alt={char.name}
+        className={styles.image}
+      />
       <p className={styles.name}>{char.name}</p>
     </div>
   ));
