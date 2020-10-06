0c0736928270ee7e1eeeadabd0ad8e626b3aac95
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-06 09:57:44 +0700

    Navbar : Input styles

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,18 +1,22 @@
-import React, { useState } from 'react'
-import SignUpToModal from './SignUp/SignUptoModal';
-import styles from "./Navbar.module.css"
-import LogoMilan from '../LogoMilan/LogoMilan';
+import React, { useState } from "react";
+import SignUpToModal from "./SignUp/SignUptoModal";
+import styles from "./Navbar.module.css";
+import LogoMilan from "../LogoMilan/LogoMilan";
 
-const Navbar = () => {        
-    return (        
-        <React.Fragment>
-            <div className={styles.container}>
-                <LogoMilan />
-                <input type="text" ></input>                     
-                {/* <SignUp />             */}
-                <SignUpToModal />
-            </div>
-        </React.Fragment>
-    )
-}
-export default Navbar
+const Navbar = () => {
+  return (
+    <React.Fragment>
+      <div className={styles.container}>
+        <LogoMilan />
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Search movies"
+        ></input>
+        {/* <SignUp />             */}
+        <SignUpToModal />
+      </div>
+    </React.Fragment>
+  );
+};
+export default Navbar;
