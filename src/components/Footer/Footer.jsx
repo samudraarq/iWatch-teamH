e998b557fc0338ee05404c498375542f19ee8733
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-08 20:00:35 +0700

    Footer : Add Social Icon

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import LogoMilan from "../LogoMilan/LogoMilan";
 import styles from "./Footer.module.css";
-import img from "./fbIcon.png";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import playStoreLogo from "./google-play-badge.png";
 import AppStoreLogo from "./Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.svg";
 
@@ -34,9 +36,9 @@ const Footer = () => {
           </div>
           <h3>Social Media</h3>
           <div className={styles.downloadSocmed}>
-            <img src={img}></img>
-            <img src={img}></img>
-            <img src={img}></img>
+            <TwitterIcon className={styles.socmedIcon} />
+            <FacebookIcon className={styles.socmedIcon} />
+            <LinkedInIcon className={styles.socmedIcon} />
           </div>
         </div>
       </div>
