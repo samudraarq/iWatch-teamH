29acb4190b7ee042602758f7b1c9cecd42d3ae60
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-09 20:37:52 +0700

    change movies api

--- src/components/BrowseMovie/MovieList/MovieList.js
+++ src/components/BrowseMovie/MovieList/MovieList.js
@@ -6,7 +6,7 @@ import styles from "./MovieList.module.css";
 
 const MovieList = ({ categories, allMovies }) => {
   const [movies, setMovies] = useState([]);
-  const [maxPage, setMaxPage] = useState(1);
+  const [maxPage, setMaxPage] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
 
   const { category } = useParams();
@@ -15,29 +15,30 @@ const MovieList = ({ categories, allMovies }) => {
     const getMovies = async () => {
       if (allMovies) {
         const res = await axios.get(
-          `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_TMDB_API_KEY}&page=${currentPage}`
+          // `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_TMDB_API_KEY}&page=${currentPage}`
+          `https://aqueous-savannah-95860.herokuapp.com/movie/${currentPage}`
         );
         const result = await res.data;
         // console.log(result.results);
-        setMaxPage(result.total_pages);
+        // setMaxPage(result.total_pages);
         setMovies(result.results);
-      } else {
-        const res = await axios.get(
-          `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&page=${currentPage}&with_genres=${category}`
-        );
-        const result = await res.data;
-        // console.log(result.results);
-        setMaxPage(result.total_pages);
-        setMovies(result.results);
+        // } else {
+        //   const res = await axios.get(
+        //     `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&page=${currentPage}&with_genres=${category}`
+        //   );
+        //   const result = await res.data;
+        //   // console.log(result.results);
+        //   setMaxPage(result.total_pages);
+        //   setMovies(result.results);
       }
     };
     getMovies();
   }, [currentPage, category, allMovies]);
 
   const movRender = movies.map((movie) => {
-    const genres = movie.genre_ids.map((id) =>
-      categories.find((obj) => obj.id === id)
-    );
+    // const genres = movie.genre_ids.map((id) =>
+    //   categories.find((obj) => obj.id === id)
+    // );
 
     return (
       <Link
@@ -53,22 +54,24 @@ const MovieList = ({ categories, allMovies }) => {
           <div className={styles.imageContainer}>
             <figure className={styles.hoverEffect}>
               <img
-                src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path}
+                // src={"https://image.tmdb.org/t/p/w500/" + movie.poster_path}
+                src={movie.img_url_poster}
                 alt="movie poster"
                 className={styles.image}
               />
               <figcaption>
                 <h2>{movie.title}</h2>
-                <p>{movie.overview.slice(0, 100)}...</p>
+                {/* <p>{movie.overview.slice(0, 100)}...</p> */}
               </figcaption>
             </figure>
           </div>
         )}
         <p className={styles.title}>{movie.title}</p>
         <div className={styles.genre}>
-          {genres?.map((genre, idx) => (
+          {/* {genres?.map((genre, idx) => (
             <span key={idx}>{genre?.name}</span>
-          ))}
+          ))} */}
+          <span>{movie.genre}</span>
         </div>
       </Link>
     );
