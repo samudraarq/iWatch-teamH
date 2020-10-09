766d3fdb8836e2718ae967f41b9e0066b02081ee
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-09 15:57:18 +0700

    modal remove button outline

--- src/components/Modal/ModalSignIn.jsx
+++ src/components/Modal/ModalSignIn.jsx
@@ -2,72 +2,75 @@ import React from "react";
 import { Field, Form, Formik, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import LogoMilan from "../LogoMilan/LogoMilan";
-import styles from './Modal.module.css'
+import styles from "./Modal.module.css";
 
-
-const ModalSignIn = ({ handleSubmit, setIsSignup, setIsLogin, handleClose }) => (        
-    <Formik    
-        initialValues={{
-        email: "",
-        password: "",
-        }}
-        
-        validationSchema={Yup.object({
-        email: Yup.string().email("Invalid email address").required("Required"),
-        password: Yup.string()
-            .min(6, "Password must be >6 characters")
-            .required("Required"),
-        })}
-        onSubmit={({ email, password }) => {                  
-            fetch('https://appdoto.herokuapp.com/api/users/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({email, password})
-            })
-            .then(res => res.json())
-            .then(result => {
-                console.log('Success:', result);
-                localStorage.setItem('token', result.data.token);
-                localStorage.setItem('username',result.data.username)
-                localStorage.setItem('isLogin', true);   
-                setIsLogin(true)             
-                handleClose()
-            }).catch((err)=>(console.log("error")))
-          }}
-      >
+const ModalSignIn = ({ setIsSignup, setIsLogin, handleClose }) => (
+  <Formik
+    initialValues={{
+      email: "",
+      password: "",
+    }}
+    validationSchema={Yup.object({
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string()
+        .min(6, "Password must be >6 characters")
+        .required("Required"),
+    })}
+    onSubmit={({ email, password }) => {
+      fetch("https://appdoto.herokuapp.com/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      })
+        .then((res) => res.json())
+        .then((result) => {
+          console.log("Success:", result);
+          localStorage.setItem("token", result.data.token);
+          localStorage.setItem("username", result.data.username);
+          localStorage.setItem("isLogin", true);
+          setIsLogin(true);
+          handleClose();
+        })
+        .catch((err) => console.log("error", err));
+    }}
+  >
     <Form>
-    <LogoMilan />
+      <LogoMilan />
       <div className={styles.inputContainer}>
-        <Field name="email" type="text" placeholder="Email Address" className={styles.input} />
+        <Field
+          name="email"
+          type="text"
+          placeholder="Email Address"
+          className={styles.input}
+        />
         <ErrorMessage name="email">
-          {
-              (msg) => (<span className={styles.error}>{msg}</span>)
-          }
+          {(msg) => <span className={styles.error}>{msg}</span>}
         </ErrorMessage>
       </div>
       <div className={styles.inputContainer}>
-        <Field name="password" type="password" placeholder="Password" className={styles.input} />
+        <Field
+          name="password"
+          type="password"
+          placeholder="Password"
+          className={styles.input}
+        />
         <ErrorMessage name="password">
-          {
-            (msg) => <span className={styles.error}>{msg}</span>
-          }
+          {(msg) => <span className={styles.error}>{msg}</span>}
         </ErrorMessage>
       </div>
       <p className={styles.switchForm}>
         {"Doesn't have an account? "}
-        <span
-          href="#"
-          variant="body2"
-          onClick={() => setIsSignup(false)}
-        >
+        <span href="#" variant="body2" onClick={() => setIsSignup(false)}>
           Sign Up
         </span>
       </p>
-      <button type="submit" className={styles.btnSubmit}>Submit</button>
+      <button type="submit" className={styles.btnSubmit}>
+        Submit
+      </button>
     </Form>
   </Formik>
-)
+);
 
 export default ModalSignIn;
--- src/components/Modal/ModalSignUp.jsx
+++ src/components/Modal/ModalSignUp.jsx
@@ -2,97 +2,93 @@ import React from "react";
 import { Field, Form, Formik, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import LogoMilan from "../LogoMilan/LogoMilan";
-import styles from './Modal.module.css'
-
+import styles from "./Modal.module.css";
 
 const ModalSignUp = ({ setIsSignup, setIsLogin, handleClose }) => (
-  <Formik    
-        initialValues={{          
-          username:"",
-          email: "",
-          password: "",  
-          fullname:"default fullname", 
-          bio:"default bio"
-        }}
-        
-        validationSchema={Yup.object({
-        username: Yup.string()
-            .min(4,"Username is too short")
-            .required("Required"),
-        email: Yup.string()
-            .email("Invalid email address")
-            .required("Required"),
-        password: Yup.string()
-            .min(6, "Password must be >6 characters")
-            .required("Required"),
-        })}
-        onSubmit={({ username, email, password, fullname, bio }) => {   
-            // e.preventDefault()       
-            fetch('https://appdoto.herokuapp.com/api/users/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({username, email, password, fullname, bio})
-            })
-            .then(res => res.json())
-            .then(result => {
-                console.log('Success:', result);
-                localStorage.setItem('token', result.data.token);
-                localStorage.setItem('username',result.data.username)
-                localStorage.setItem('isLogin', true);
-                handleClose()
-                setIsLogin(true)
-            }).catch((err)=>(console.log("error")))
-
-        }}
-      >
-        
+  <Formik
+    initialValues={{
+      username: "",
+      email: "",
+      password: "",
+      fullname: "default fullname",
+      bio: "default bio",
+    }}
+    validationSchema={Yup.object({
+      username: Yup.string()
+        .min(4, "Username is too short")
+        .required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string()
+        .min(6, "Password must be >6 characters")
+        .required("Required"),
+    })}
+    onSubmit={({ username, email, password, fullname, bio }) => {
+      // e.preventDefault()
+      fetch("https://appdoto.herokuapp.com/api/users/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, email, password, fullname, bio }),
+      })
+        .then((res) => res.json())
+        .then((result) => {
+          console.log("Success:", result);
+          localStorage.setItem("token", result.data.token);
+          localStorage.setItem("username", result.data.username);
+          localStorage.setItem("isLogin", true);
+          handleClose();
+          setIsLogin(true);
+        })
+        .catch((err) => console.log("error", err));
+    }}
+  >
     <Form>
-    <LogoMilan />
+      <LogoMilan />
       <div className={styles.inputContainer}>
-        <Field name="username" type="text" placeholder="Username" className={styles.input}/>
+        <Field
+          name="username"
+          type="text"
+          placeholder="Username"
+          className={styles.input}
+        />
         <ErrorMessage name="username">
-          {
-            (msg) =><span className={styles.error}>{msg}</span>
-          }
+          {(msg) => <span className={styles.error}>{msg}</span>}
         </ErrorMessage>
       </div>
-      
       <div className={styles.inputContainer}>
-        <Field name="email" type="text" placeholder="Email Address" className={styles.input}/>
+        <Field
+          name="email"
+          type="text"
+          placeholder="Email Address"
+          className={styles.input}
+        />
         <ErrorMessage name="email">
-          {
-            (msg) => <span className={styles.error}>{msg}</span>
-          }
+          {(msg) => <span className={styles.error}>{msg}</span>}
         </ErrorMessage>
       </div>
-      
-
-      <div className={styles.inputContainer}> 
-        <Field name="password" type="password" placeholder="Password" className={styles.input}/>      
+      <div className={styles.inputContainer}>
+        <Field
+          name="password"
+          type="password"
+          placeholder="Password"
+          className={styles.input}
+        />
         <ErrorMessage name="password">
-          {
-            (msg) => <span className={styles.error}>{msg}</span>
-          }
+          {(msg) => <span className={styles.error}>{msg}</span>}
         </ErrorMessage>
       </div>
-
       <p className={styles.switchForm}>
         {"Already have an account? "}
-        <span
-          href="#"
-          variant="body2"
-          onClick={() => setIsSignup(true)}
-        >
+        <span href="#" variant="body2" onClick={() => setIsSignup(true)}>
           Sign in
         </span>
       </p>
-      <button type="submit" className={styles.btnSubmit}>Submit</button>
+      <button type="submit" className={styles.btnSubmit}>
+        Submit
+      </button>
     </Form>
   </Formik>
-)
-
-
+);
 
 export default ModalSignUp;
