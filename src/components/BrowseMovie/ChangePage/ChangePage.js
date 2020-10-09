2280f3a249c084b836b42589afec151a1c379c06
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-09 08:37:00 +0700

    Hide carousel on small device

--- src/components/BrowseMovie/ChangePage/ChangePage.js
+++ src/components/BrowseMovie/ChangePage/ChangePage.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ReactPaginate from "react-paginate";
 import styles from "./ChangePage.module.css";
+import useWindowSize from "../../Hooks/useWindowResize"
 
 const ChangePage = ({ pageChange, maxPage }) => {
+  const [smallDevice, setSmallDevice] = useState(false)
+
+  const [width, height] = useWindowSize();
+
+  useEffect(() => {
+    if (width < 600) {
+      setSmallDevice(true);
+    } else {
+      setSmallDevice(false);
+    }
+  }, [width]);
+
   return (
     <div className={styles.paginationContainer}>
       <ReactPaginate
@@ -10,7 +23,7 @@ const ChangePage = ({ pageChange, maxPage }) => {
         nextLabel={"next"}
         pageCount={maxPage}
         marginPagesDisplayed={1}
-        pageRangeDisplayed={5}
+        pageRangeDisplayed={smallDevice ? 2 : 5 }
         onPageChange={pageChange}
         containerClassName={styles.pagination}
         activeClassName={styles.active}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BrowseMovie from "../components/BrowseMovie/BrowseMovie";
 import Carousel from "../components/Carousel/Carousel";
 import Footer from "../components/Footer/Footer"
+import useWindowSize from "../components/Hooks/useWindowResize";
 import Navbar from "../components/NavBar/Navbar"
 
+
 const Home = () => {
+  const [smallDevice, setSmallDevice] = useState(false)
+
+  const [width, height] = useWindowSize();
+
+  useEffect(() => {
+    if (width < 600) {
+      setSmallDevice(true);
+    } else {
+      setSmallDevice(false);
+    }
+  }, [width]);
+
   return (
     <div>      
       <Navbar />
-      <Carousel />
+      {!smallDevice && <Carousel />}
       <BrowseMovie />
       <Footer />
     </div>
