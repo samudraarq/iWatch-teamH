533015edbacf525b5d35778a75d964bb0789aed0
Samudra Faris Arqam <samudrafaris@gmail.com>
2020-10-12 09:01:02 +0700

    profpic

--- src/components/NavBar/FotoProfil.jsx
+++ src/components/NavBar/FotoProfil.jsx
@@ -3,9 +3,13 @@ import { Avatar } from "@material-ui/core";
 import { UserContext } from "../Context/UserContext";
 
 const FotoProfil = () => {
-  const { username } = useContext(UserContext);
+  const { username, userImg } = useContext(UserContext);
 
-  return <Avatar alt="User Profile Picture">{username[0]}</Avatar>;
+  return (
+    <Avatar alt="User Profile Picture" src={userImg ? userImg : ""}>
+      {username[0]}
+    </Avatar>
+  );
 };
 
 export default FotoProfil;
